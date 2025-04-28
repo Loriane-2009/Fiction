08623273d56172c0bca87a79753e43ebe080562b
Loriane Magnenat <loriane@MacBook-Pro-de-Loriane.local>
2025-04-28 12:02:24 +0200

    Connexion entre le frontend et le backend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Liste des histoires</h1>
+    <ul>
+      <li v-for="story in stories" :key="story.id">
+        {{ story.title }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import api from '../services/api';
+
+const stories = ref([]);
+
+onMounted(async () => {
+  const response = await api.get('/stories');
+  stories.value = response.data;
+});
+</script>
